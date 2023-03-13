33239e635dc4f781df2837cb34baf5dd7cf44bee
YoiseauDesIles <johan.labrado@hotmail.fr>
2023-03-13 23:26:25 +0100

    tests parser Json

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/filereadstream.h"
+
+int jsonReaderFromFile();
+
+int main()
+{
+    std::cout << jsonReaderFromFile() << "\n";
+}
+
+
+
+void jsonParserWithBuffer()
+{
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    rapidjson::Document d;
+    d.Parse(json);
+
+    rapidjson::Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    std::cout << "jsonParserWithBuffer : \n" << buffer.GetString() << "\n";
+}
+
+int jsonReaderFromFile()
+{
+    //open the file
+    std::FILE * file = fopen("details.json", "rb");
+
+    if (!file) {
+        std::cerr << "Error: unable to open file"
+                  << std::endl;
+        return 1;
+    }
+
+    //Read the file into a buffer
+    char readBuffer[65536];
+    rapidjson::FileReadStream is(file, readBuffer, sizeof(readBuffer));
+
+    //Parse the Json documentument
+    rapidjson::Document document;
+    document.ParseStream(is);
+
+    // Check if the documentument is valid
+    if (document.HasParseError()) {
+        std::cerr << "Error: failed to parse JSON documentument"
+                  << std::endl;
+        fclose(file);
+        return 1;
+    }
+    
+
+    //close the file
+    fclose(file);
+
+// Get the "name" member
+    if (document.HasMember("name") && document["name"].IsString()) {
+        std::string name = document["name"].GetString();
+        std::cout << "Name: " << name << std::endl;
+    }
+  
+    // Get the "age" member
+    if (document.HasMember("age") && document["age"].IsInt()) {
+        int age = document["age"].GetInt();
+        std::cout << "Age: " << age << std::endl;
+    }
+  
+    // Get the "city" member
+    if (document.HasMember("city") && document["city"].IsString()) {
+        std::string city = document["city"].GetString();
+        std::cout << "City: " << city << std::endl;
+    }
+  
+    // Get the "skills" array
+    if (document.HasMember("skills")
+        && document["skills"].IsArray()) {
+        const rapidjson::Value& skills = document["skills"];
+        std::cout << "Skills: ";
+        for (rapidjson::SizeType i = 0; i < skills.Size();
+             i++) {
+            if (skills[i].IsString()) {
+                std::string skill = skills[i].GetString();
+                std::cout << skill << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+  
+
+    return 0;
+
+}
